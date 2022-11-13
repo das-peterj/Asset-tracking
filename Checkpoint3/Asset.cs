d8c9fa60dca7274f9d29088cae0b21bb87e03307
Peter Jorgensen <dasbomber.jorgensen@gmail.com>
2022-11-13 22:00:53 +0100

    Extracted base class to seperate file

---
+++ Checkpoint3/Asset.cs
@@ -0,0 +1,25 @@
+﻿// Declares the list and puts in sample data for easier testing
+
+
+
+
+class Asset
+{
+    public string Type { get; set; }
+    public string Brand { get; set; }
+    public string Model { get; set; }
+    public string Office { get; set; }
+
+    public int PriceInUSD { get; set; }
+    public DateTime PurchaseDate { get; set; }
+
+    public Asset(string type, string brand, string model, string office, DateTime purchaseDate, int priceInUSD)
+    {
+        Type = type;
+        Brand = brand;
+        Model = model;
+        Office = office;
+        PurchaseDate = purchaseDate;
+        PriceInUSD = priceInUSD;
+    }
+}
--- Checkpoint3/Program.cs
+++ Checkpoint3/Program.cs
@@ -200,25 +200,3 @@ string checkLocalCurrency(string input)
         return "USD";
     }
 }
-
-
-class Asset
-{
-    public string Type { get; set; }
-    public string Brand { get; set; }
-    public string Model { get; set; }
-    public string Office { get; set; }
-
-    public int PriceInUSD { get; set; }
-    public DateTime PurchaseDate { get; set; }
-
-    public Asset(string type, string brand, string model, string office, DateTime purchaseDate, int priceInUSD)
-    {
-        Type = type;
-        Brand = brand;
-        Model = model;
-        Office = office;
-        PurchaseDate = purchaseDate;
-        PriceInUSD = priceInUSD;
-    }
-}
